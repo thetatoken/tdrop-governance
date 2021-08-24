6b6af94d000d7c1cf461eafcce5e03c3dbcb77b6
Jieyi Long <jieyi@sliver.tv>
2021-08-24 19:54:52 +0800

    TDrop token initial commit

--- contracts/TDrop.sol
+++ contracts/TDrop.sol
@@ -3,30 +3,31 @@ pragma experimental ABIEncoderV2;
 
 import "./SafeMath.sol";
 
-contract Uni {
+contract TDrop {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Uniswap";
+    string public constant name = "TDrop";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "UNI";
+    string public constant symbol = "TDROP";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
 
+    /// @notice Max number of tokens
+    // Note that 2**96 > 20 * 10**9 * 10**18, therefore it is safe to use uint96 to represent the TDrop token (2**96/10**18 is roughly 79 billion).
+    uint public maxSupply = 20_000_000_000e18; // 20 billion TDrop. 
+
     /// @notice Total number of tokens in circulation
-    uint public totalSupply = 1_000_000_000e18; // 1 billion Uni
+    uint public totalSupply = 0; 
+
+    /// @notice Address which may mint new tokens
+    address public admin;
 
     /// @notice Address which may mint new tokens
     address public minter;
 
     /// @notice The timestamp after which minting may occur
     uint public mintingAllowedAfter;
-
-    /// @notice Minimum time between mints
-    uint32 public constant minimumTimeBetweenMints = 1 days * 365;
-
-    /// @notice Cap on the percentage of totalSupply that can be minted at each mint
-    uint8 public constant mintCap = 2;
 
     /// @notice Allowance amounts on behalf of others
     mapping (address => mapping (address => uint96)) internal allowances;
@@ -61,6 +62,9 @@ contract Uni {
     /// @notice A record of states for signing / validating signatures
     mapping (address => uint) public nonces;
 
+    /// @notice An event thats emitted when the admin address is changed
+    event AdminChanged(address admin, address newAdmin);
+
     /// @notice An event thats emitted when the minter address is changed
     event MinterChanged(address minter, address newMinter);
 
@@ -77,14 +81,16 @@ contract Uni {
     event Approval(address indexed owner, address indexed spender, uint256 amount);
 
     /**
-     * @notice Construct a new Uni token
+     * @notice Construct a new TDrop token
      * @param account The initial account to grant all the tokens
      * @param minter_ The account with minting ability
      * @param mintingAllowedAfter_ The timestamp after which minting may occur
      */
     constructor(address account, address minter_, uint mintingAllowedAfter_) public {
-        require(mintingAllowedAfter_ >= block.timestamp, "Uni::constructor: minting can only begin after deployment");
+        require(mintingAllowedAfter_ >= block.timestamp, "TDrop::constructor: minting can only begin after deployment");
 
+        admin = msg.sender;
+        emit AdminChanged(address(0), admin);
         balances[account] = uint96(totalSupply);
         emit Transfer(address(0), account, totalSupply);
         minter = minter_;
@@ -93,11 +99,21 @@ contract Uni {
     }
 
     /**
+     * @notice Change the admin address
+     * @param admin_ The address of the new admin
+     */
+    function setAdmin(address admin_) external {
+        require(msg.sender == admin, "TDrop::setMinter: only the admin can change the admin address");
+        emit AdminChanged(admin, admin_);
+        admin = admin_;
+    }
+
+    /**
      * @notice Change the minter address
      * @param minter_ The address of the new minter
      */
     function setMinter(address minter_) external {
-        require(msg.sender == minter, "Uni::setMinter: only the minter can change the minter address");
+        require(msg.sender == admin, "TDrop::setMinter: only the admin can change the minter address");
         emit MinterChanged(minter, minter_);
         minter = minter_;
     }
@@ -108,24 +124,36 @@ contract Uni {
      * @param rawAmount The number of tokens to be minted
      */
     function mint(address dst, uint rawAmount) external {
-        require(msg.sender == minter, "Uni::mint: only the minter can mint");
-        require(block.timestamp >= mintingAllowedAfter, "Uni::mint: minting not allowed yet");
-        require(dst != address(0), "Uni::mint: cannot transfer to the zero address");
-
-        // record the mint
-        mintingAllowedAfter = SafeMath.add(block.timestamp, minimumTimeBetweenMints);
+        require(msg.sender == minter, "TDrop::mint: only the minter can mint");
+        require(block.timestamp >= mintingAllowedAfter, "TDrop::mint: minting not allowed yet");
+        require(dst != address(0), "TDrop::mint: cannot transfer to the zero address");
 
         // mint the amount
-        uint96 amount = safe96(rawAmount, "Uni::mint: amount exceeds 96 bits");
-        require(amount <= SafeMath.div(SafeMath.mul(totalSupply, mintCap), 100), "Uni::mint: exceeded mint cap");
-        totalSupply = safe96(SafeMath.add(totalSupply, amount), "Uni::mint: totalSupply exceeds 96 bits");
+        uint96 amount = safe96(rawAmount, "TDrop::mint: amount exceeds 96 bits");
+        totalSupply = safe96(SafeMath.add(totalSupply, amount), "TDrop::mint: totalSupply exceeds 96 bits");
+        require(totalSupply <= maxSupply, "TDrop::mint: totalSupply exceeds maxSupply");
 
         // transfer the amount to the recipient
-        balances[dst] = add96(balances[dst], amount, "Uni::mint: transfer amount overflows");
+        balances[dst] = add96(balances[dst], amount, "TDrop::mint: transfer amount overflows");
         emit Transfer(address(0), dst, amount);
 
         // move delegates
         _moveDelegates(address(0), delegates[dst], amount);
+    }
+
+    /**
+     * @notice Airdrop tokens for the given list of addresses
+     * @param dsts The addresses of the destination accounts
+     * @param rawAmounts The number of tokens to be airdropped to each destination account
+     */
+    function airdrop(address[] calldata dsts, uint[] calldata rawAmounts) external {
+        require(dsts.length == rawAmounts.length);
+        uint numDsts = dsts.length;
+        for (uint i = 0; i < numDsts; i ++) {
+            address dst = dsts[i];
+            uint rawAmount = rawAmounts[i];
+            this.mint(dst, rawAmount);
+        }
     }
 
     /**
@@ -151,7 +179,7 @@ contract Uni {
         if (rawAmount == uint(-1)) {
             amount = uint96(-1);
         } else {
-            amount = safe96(rawAmount, "Uni::approve: amount exceeds 96 bits");
+            amount = safe96(rawAmount, "TDrop::approve: amount exceeds 96 bits");
         }
 
         allowances[msg.sender][spender] = amount;
@@ -175,16 +203,16 @@ contract Uni {
         if (rawAmount == uint(-1)) {
             amount = uint96(-1);
         } else {
-            amount = safe96(rawAmount, "Uni::permit: amount exceeds 96 bits");
+            amount = safe96(rawAmount, "TDrop::permit: amount exceeds 96 bits");
         }
 
         bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), getChainId(), address(this)));
         bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, rawAmount, nonces[owner]++, deadline));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "Uni::permit: invalid signature");
-        require(signatory == owner, "Uni::permit: unauthorized");
-        require(now <= deadline, "Uni::permit: signature expired");
+        require(signatory != address(0), "TDrop::permit: invalid signature");
+        require(signatory == owner, "TDrop::permit: unauthorized");
+        require(now <= deadline, "TDrop::permit: signature expired");
 
         allowances[owner][spender] = amount;
 
@@ -207,7 +235,7 @@ contract Uni {
      * @return Whether or not the transfer succeeded
      */
     function transfer(address dst, uint rawAmount) external returns (bool) {
-        uint96 amount = safe96(rawAmount, "Uni::transfer: amount exceeds 96 bits");
+        uint96 amount = safe96(rawAmount, "TDrop::transfer: amount exceeds 96 bits");
         _transferTokens(msg.sender, dst, amount);
         return true;
     }
@@ -222,10 +250,10 @@ contract Uni {
     function transferFrom(address src, address dst, uint rawAmount) external returns (bool) {
         address spender = msg.sender;
         uint96 spenderAllowance = allowances[src][spender];
-        uint96 amount = safe96(rawAmount, "Uni::approve: amount exceeds 96 bits");
+        uint96 amount = safe96(rawAmount, "TDrop::approve: amount exceeds 96 bits");
 
         if (spender != src && spenderAllowance != uint96(-1)) {
-            uint96 newAllowance = sub96(spenderAllowance, amount, "Uni::transferFrom: transfer amount exceeds spender allowance");
+            uint96 newAllowance = sub96(spenderAllowance, amount, "TDrop::transferFrom: transfer amount exceeds spender allowance");
             allowances[src][spender] = newAllowance;
 
             emit Approval(src, spender, newAllowance);
@@ -257,9 +285,9 @@ contract Uni {
         bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "Uni::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "Uni::delegateBySig: invalid nonce");
-        require(now <= expiry, "Uni::delegateBySig: signature expired");
+        require(signatory != address(0), "TDrop::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "TDrop::delegateBySig: invalid nonce");
+        require(now <= expiry, "TDrop::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -281,7 +309,7 @@ contract Uni {
      * @return The number of votes the account had as of the given block
      */
     function getPriorVotes(address account, uint blockNumber) public view returns (uint96) {
-        require(blockNumber < block.number, "Uni::getPriorVotes: not yet determined");
+        require(blockNumber < block.number, "TDrop::getPriorVotes: not yet determined");
 
         uint32 nCheckpoints = numCheckpoints[account];
         if (nCheckpoints == 0) {
@@ -325,11 +353,11 @@ contract Uni {
     }
 
     function _transferTokens(address src, address dst, uint96 amount) internal {
-        require(src != address(0), "Uni::_transferTokens: cannot transfer from the zero address");
-        require(dst != address(0), "Uni::_transferTokens: cannot transfer to the zero address");
+        require(src != address(0), "TDrop::_transferTokens: cannot transfer from the zero address");
+        require(dst != address(0), "TDrop::_transferTokens: cannot transfer to the zero address");
 
-        balances[src] = sub96(balances[src], amount, "Uni::_transferTokens: transfer amount exceeds balance");
-        balances[dst] = add96(balances[dst], amount, "Uni::_transferTokens: transfer amount overflows");
+        balances[src] = sub96(balances[src], amount, "TDrop::_transferTokens: transfer amount exceeds balance");
+        balances[dst] = add96(balances[dst], amount, "TDrop::_transferTokens: transfer amount overflows");
         emit Transfer(src, dst, amount);
 
         _moveDelegates(delegates[src], delegates[dst], amount);
@@ -340,21 +368,21 @@ contract Uni {
             if (srcRep != address(0)) {
                 uint32 srcRepNum = numCheckpoints[srcRep];
                 uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint96 srcRepNew = sub96(srcRepOld, amount, "Uni::_moveVotes: vote amount underflows");
+                uint96 srcRepNew = sub96(srcRepOld, amount, "TDrop::_moveVotes: vote amount underflows");
                 _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
             }
 
             if (dstRep != address(0)) {
                 uint32 dstRepNum = numCheckpoints[dstRep];
                 uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint96 dstRepNew = add96(dstRepOld, amount, "Uni::_moveVotes: vote amount overflows");
+                uint96 dstRepNew = add96(dstRepOld, amount, "TDrop::_moveVotes: vote amount overflows");
                 _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
             }
         }
     }
 
     function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
-      uint32 blockNumber = safe32(block.number, "Uni::_writeCheckpoint: block number exceeds 32 bits");
+      uint32 blockNumber = safe32(block.number, "TDrop::_writeCheckpoint: block number exceeds 32 bits");
 
       if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
           checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
